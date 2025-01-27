6fecf57a469113c2c1618b43d340596f2b59d794
Amulya <>
2025-01-27 10:32:05 -0500

    Refactor: Simplify AWS EC2 Instance Resource

--- resources.tf
+++ resources.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "web" {
-   count = 1 
-   ami  = "${var.ami}"
-   instance_type = "t2.micro"
-   key_name = "${var.key_name}"
-   subnet_id = element([aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id, aws_subnet.public-subnet-3.id], count.index)
-   vpc_security_group_ids = ["${aws_security_group.sg-web.id}"]
-   associate_public_ip_address = true
-  #  user_data = "${file("install.sh")}"
-   tags = {
-     Name = "webserver"
-   }
-}
+  count                     = 1
+  ami                       = var.ami
+  instance_type             = "t2.micro"
+  key_name                  = var.key_name
+  subnet_id                 = element(aws_subnet.public-subnets[*].id, count.index)
+  vpc_security_group_ids    = [aws_security_group.sg-web.id]
+  associate_public_ip_address = true
+  user_data                 = file("install.sh")
 
+  tags = {
+    Name = "webserver"
+  }
+}
