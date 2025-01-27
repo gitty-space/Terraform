e7ed1a1e54c36dc70d9f0e8665b226936846a70a
Amulya <>
2025-01-26 23:21:39 -0500

    Create Security Group to allow incoming HTTP Connections and SSH Access

--- vpc.tf
+++ vpc.tf
@@ -118,3 +118,102 @@ resource "aws_route_table_association" "web-public-rt-assoc_3" {
   subnet_id      = aws_subnet.public-subnet-3.id
   route_table_id = aws_route_table.web-public-rt.id
 }
+
+resource "aws_security_group" "sg-web" {
+  name = "dev-sg-public-sg-web"
+  description = "Allow incoming HTTP connections & SSH access"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id="${aws_vpc.vpc-dev.id}"
+
+  tags = {
+    Name = "Web Server SG"
+    application = "Demo App"
+  }
+}
+
+resource "aws_security_group" "sg-pub"{
+  name = "dev-sg-private-sg-pub"
+  description = "Allow traffic from public subnets"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [
+      aws_subnet.public-subnet-1.cidr_block,
+      aws_subnet.public-subnet-2.cidr_block,
+      aws_subnet.public-subnet-3.cidr_block
+    ]
+  }
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = [
+      aws_subnet.public-subnet-1.cidr_block,
+      aws_subnet.public-subnet-2.cidr_block,
+      aws_subnet.public-subnet-3.cidr_block
+    ]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [
+      aws_subnet.public-subnet-1.cidr_block,
+      aws_subnet.public-subnet-2.cidr_block,
+      aws_subnet.public-subnet-3.cidr_block
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = "${aws_vpc.vpc-dev.id}"
+
+  tags = {
+    Name = "dev-sg-private-pub"
+    application = "Demo App"
+  }
+}
